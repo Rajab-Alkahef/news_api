30a75c3d4bb077cb84be9ba6dda15408a6761ab0
Rajab-Alkahef <rajabalkahef3@gmail.com>
2023-09-09 11:43:13 +0300

    create news app

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -7,43 +7,30 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          centerTitle: true,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "News",
-                style: TextStyle(
-                  color: Color(0xff000000),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            centerTitle: true,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "News",
+                  style: TextStyle(
+                    color: Color(0xff000000),
+                  ),
                 ),
-              ),
-              Text(
-                "Cloud",
-                style: TextStyle(color: Colors.orange),
-              ),
-            ],
-          ),
-        ),
-        body: SizedBox(
-          height: 30,
-          child: ListView.builder(
-            itemCount: 5,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: ((context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 100,
-                  color: Colors.black,
+                Text(
+                  "Cloud",
+                  style: TextStyle(color: Colors.orange),
                 ),
-              );
-            }),
+              ],
+            ),
           ),
-        ),
-      ),
+          body: Container(
+            height: 100,
+            color: Colors.amber,
+          )),
     );
   }
 }
