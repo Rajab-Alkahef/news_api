e9ba114ab5420197e1a5487f42b2cb2d95c53994
Rajab-Alkahef <rajabalkahef3@gmail.com>
2023-09-15 15:57:37 +0300

    Future builder finish and add cache networlk image

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,32 +1,45 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/model/news_model.dart';
 import 'package:news_app_ui_setup/widgets/news_list_view.dart';
 
 import '../services/news_services.dart';
 
-class NewsListViewBuilder extends StatelessWidget {
+class NewsListViewBuilder extends StatefulWidget {
   const NewsListViewBuilder({super.key});
 
   @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
+  var future;
+  @override
+  void initState() {
+    super.initState();
+    future = NewsService(Dio()).getGeneralNews();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: NewsService(Dio()).getGeneralNews(),
-        builder: (context, snapshot) {
+    return FutureBuilder<List<NewsModel>>(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
           return NewsListview(
-            articles: snapshot.data ?? [],
+            articles: snapshot.data!,
           );
-        });
-    // isLoadig
-    //     ? const SliverFillRemaining(
-    //         hasScrollBody: false,
-    //         child: Center(child: CircularProgressIndicator()),
-    //       )
-    //     : articles.isEmpty
-    //         ? NewsListview(
-    //             articles: articles,
-    //           )
-    //         : const SliverToBoxAdapter(
-    //             child: Text("There is an error, try again later"),
-    //           );
+        } else if (snapshot.hasError) {
+          return const SliverToBoxAdapter(
+            child: Text("There is an error, try again later"),
+          );
+        } else {
+          return const SliverFillRemaining(
+            hasScrollBody: false,
+            child: Center(child: CircularProgressIndicator()),
+          );
+        }
+      },
+    );
   }
 }
--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app_ui_setup/model/news_model.dart';
 
@@ -12,14 +13,31 @@ class NewsTile extends StatelessWidget {
         ClipRRect(
           borderRadius: BorderRadius.circular(6),
           child: news.image != null
-              ? Image.network(
-                  news.image!,
-                  scale: 1.0,
+              ? CachedNetworkImage(
                   height: 200,
                   width: double.infinity,
-                  fit: BoxFit.cover,
+                  imageUrl: news.image!,
+                  placeholder: (context, url) => const SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: Center(child: CircularProgressIndicator()),
+                  ),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
                 )
-              : const Text('image not found'),
+              : const SizedBox(
+                  height: 200,
+                  width: double.infinity,
+                  child: Center(
+                    child: Text('image not found'),
+                  ),
+                ),
+          // Image.network(
+          //     news.image!,
+          //     scale: 1.0,
+          //     height: 200,
+          //     width: double.infinity,
+          //     fit: BoxFit.cover,
+          //   )
         ),
         const SizedBox(
           height: 12,
