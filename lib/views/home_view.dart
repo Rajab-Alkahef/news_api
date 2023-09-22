a472e4510528be39b5ecf4f61509d14fce68f813
Rajab-Alkahef <rajabalkahef3@gmail.com>
2023-09-22 18:55:00 +0300

    refactor UI

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:news_app_ui_setup/widgets/categories_list_view.dart';
+import 'package:news_app_ui_setup/widgets/home_view_builder.dart';
 import 'package:news_app_ui_setup/widgets/news_list_view_builder.dart';
 import 'package:news_app_ui_setup/widgets/search_bar.dart';
 
@@ -53,52 +54,6 @@ class HomeView extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 12.0),
           child: HomeViewBuilder(isSearching: isSearching),
         ),
-      ),
-    );
-  }
-}
-
-class HomeViewBuilder extends StatefulWidget {
-  const HomeViewBuilder({
-    super.key,
-    required this.isSearching,
-  });
-  final bool isSearching;
-
-  @override
-  State<HomeViewBuilder> createState() => _HomeViewBuilderState();
-}
-
-class _HomeViewBuilderState extends State<HomeViewBuilder> {
-  @override
-  Widget build(BuildContext context) {
-    return RefreshIndicator(
-      color: Colors.orange,
-      onRefresh: () async {
-        Future.delayed(const Duration(seconds: 3));
-        print('refreshed');
-        const NewsListViewBuilder(
-          category: 'general',
-        );
-        setState(() {});
-      },
-      child: CustomScrollView(
-        physics: const BouncingScrollPhysics(),
-        slivers: [
-          widget.isSearching == true
-              ? const SliverToBoxAdapter(
-                  child: CustomSearchBar(),
-                )
-              : const SliverToBoxAdapter(child: CategoriesListView()),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 32,
-            ),
-          ),
-          const NewsListViewBuilder(
-            category: 'general',
-          ),
-        ],
       ),
     );
   }
